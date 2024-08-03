fc00f6d3340d3102f5cee2b1e6e69cfc649bc88b
chaeminyu <chaeminyu>
2024-08-03 16:38:08 +0900

    [FIX] 회원가입 및 조회 시 멘토/멘티 프로필 반환

--- users/serializers.py
+++ users/serializers.py
@@ -9,13 +9,15 @@ class InterestSerializer(serializers.ModelSerializer):
 
 class MentorSerializer(serializers.ModelSerializer):
   # Write-only field to receive interests as choice names
-  interests = serializers.ListField(
-    child=serializers.ChoiceField(choices=[choice[0] for choice in Interest.INTEREST_CHOICES]),  # 선택 항목의 키 값만 사용
-    write_only=True
-  )
+  # interests = serializers.ListField(
+  #   child=serializers.ChoiceField(choices=[choice[0] for choice in Interest.INTEREST_CHOICES]),  # 선택 항목의 키 값만 사용
+  #   write_only=True
+  # )
+  interests = serializers.MultipleChoiceField(choices=Interest.INTEREST_CHOICES, write_only=True)
+  interests_display = serializers.SerializerMethodField()
   mentor_name = serializers.CharField(source='user.name', read_only=True)
   # Read-only field to display interests
-  interests_display = InterestSerializer(source='interests', many=True, read_only=True)
+  #interests_display = InterestSerializer(source='interests', many=True, read_only=True)
 
   class Meta:
     model = Mentor
@@ -28,7 +30,9 @@ class MentorSerializer(serializers.ModelSerializer):
       'rating',
       'total_ratings'
     ]
-    read_only_fields = ['id', 'user', 'mentor_name']
+    read_only_fields = ['id', 'user']
+  def get_interests_display(self, obj):
+    return [{'name': interest.name} for interest in obj.interests.all()]
 
   def validate_interests(self, value):
     if len(value) > 3:
@@ -51,8 +55,8 @@ class MenteeSerializer(serializers.ModelSerializer):
     read_only_fields = ['id', 'user']
 
 class UserSerializer(serializers.ModelSerializer):
-  mentor_profile = MentorSerializer(source='mentor', read_only=True) #근데 리드온리로 하면 나중에 수정 못하는거 아냐?
-  mentee_profile = MenteeSerializer(source='mentee', read_only=True)
+  mentor_profile = MentorSerializer(source='mentor', required=False) #근데 리드온리로 하면 나중에 수정 못하는거 아냐?
+  mentee_profile = MenteeSerializer(source='mentee', required=False)
 
   class Meta:
     model = User
@@ -69,12 +73,19 @@ class UserSerializer(serializers.ModelSerializer):
     }
 
   def create(self, validated_data):
-    # nested profile data 가져오기
-    mentor_data = validated_data.pop('mentor_profile', None)
-    mentee_data = validated_data.pop('mentee_profile', None)
+    # # nested profile data 가져오기
+    # mentor_data = validated_data.pop('mentor_profile', None)
+    # mentee_data = validated_data.pop('mentee_profile', None)
 
-    # hashed password 가지고 유저 생성
-    user = User.objects.create_user(**validated_data)
+    # # hashed password 가지고 유저 생성
+    # user = User.objects.create_user(**validated_data)
+    mentor_data = validated_data.pop('mentor', None)
+    mentee_data = validated_data.pop('mentee', None)
+
+    password = validated_data.pop('password')
+    user = User(**validated_data)
+    user.set_password(password)
+    user.save()
 
     # Mentor 생성
     if user.is_mentor and mentor_data:
@@ -83,14 +94,16 @@ class UserSerializer(serializers.ModelSerializer):
       # MentorInterest를 통해 interest 넣기
       for interest_name in interests_data:
         interest = Interest.objects.get(name=interest_name)
-        MentorInterest.objects.create(mentor=mentor, interest=interest)
+        mentor.interests.add(interest)
+        # MentorInterest.objects.create(mentor=mentor, interest=interest)
 
     # Mentee 생성
-    if not user.is_mentor:
-      if mentee_data is None:
-        mentee_data = {}
+    elif not user.is_mentor:
+      # if mentee_data is None:
+      #   mentee_data = {}
       # 멘티는 카테고리를 설정하지 않는다는 새로운 설정!
-      Mentee.objects.create(user=user, **mentee_data)
+      # Mentee.objects.create(user=user, **mentee_data)
+      Mentee.objects.create(user=user, **(mentee_data or {}))
     return user
 
   def update(self, instance, validated_data):
