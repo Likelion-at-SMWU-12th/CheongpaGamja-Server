6356db7910f30a05c8b556b35b9d358809e89ab1
chaeminyu <chaeminyu>
2024-08-05 10:56:08 +0900

    [FIX] 칼럼 이미지 업로드, 멘토별 칼럼 조회

--- community/urls.py
+++ community/urls.py
@@ -1,10 +1,9 @@
-# community/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ColumnViewSet
 
 router = DefaultRouter()
-router.register(r'columns', ColumnViewSet)
+router.register(r'columns', ColumnViewSet, basename='column')
 
 urlpatterns = [
     path('', include(router.urls)),
