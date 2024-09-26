e1a43b1dbbe05f847bfbd6a29dce47305027a98a
chaeminyu <chaeminyu>
2024-09-26 22:52:24 +0900

    [FIX] 클라이언트 주소 변경

--- voyage/settings.py
+++ voyage/settings.py
@@ -94,7 +94,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://43.202.48.216:3000/')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://43.202.48.216:3000')
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'voyage.urls'
